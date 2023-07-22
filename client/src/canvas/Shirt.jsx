2d6118734c8ea45036bad0e046d318b3eef7b2d7
Dipen Chauhan <get.dipen@gmail.com>
2023-07-22 01:15:19 -0700

    displaying the shirt from glb file

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import state from "../store";
 const Shirt = () => {
+  const snap = useSnapshot(state);
+  // Import the Shirt 3D model
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
+  // Apply texture
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
   return (
-    <div>Shirt</div>
+    <group>
+      <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} dispose={null}>
+
+      </mesh>
+    </group>
   )
 }
 
