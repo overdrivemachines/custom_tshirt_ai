ae3dcbabdd0b564fec6bc4828361364866eeec58
Dipen Chauhan <get.dipen@gmail.com>
2023-07-22 00:59:47 -0700

    adding components

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import {Canvas} from "@react-three/fiber";
+import {Environment, Center} from "@react-three/drei";
 
-const Canvas = () => {
+// Import from ./canvas/*
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+import Shirt from "./Shirt";
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      {/* Apply ambient lighting */}
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+/
+      {/* <CameraRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+      <Shirt />
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
