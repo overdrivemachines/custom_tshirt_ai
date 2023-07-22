ce90cd3813c11f70291a818a5e46900701e450ae
Dipen Chauhan <get.dipen@gmail.com>
2023-07-22 15:48:32 -0700

    added light and shadow

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -8,13 +8,14 @@ import Shirt from "./Shirt";
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    // fov is like zoom
+    <Canvas shadows camera={{position: [0,0,0], fov: 25}} gl={{preserveDrawingBuffer: true}} className="w-full max-w-full h-full transition-all ease-in">
       {/* Apply ambient lighting */}
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
         {/* glow behind the shirt */}
-        {/* <Backdrop /> */}
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
