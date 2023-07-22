5f09c6e8c68e9be7d0e80c10f41d639481529a97
Dipen Chauhan <get.dipen@gmail.com>
2023-07-21 20:43:49 -0700

    display tabs, filter toggles and go back button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config'
+import state from '../store'
+import {download} from '../assets'
+import {downloadCanvasToImage, reader} from '../config/helpers';
+import {EditorTabs, FilterTabs, DecalTypes} from '../config/constants'
+import {fadeAnimation, slideAnimation} from '../config/motion'
+
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          {/* Tabs on the left */}
+          <motion.div key="custom" className='absolute top-0 left-0 z-10' {...slideAnimation('left')}>
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map(tab => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}}></Tab>
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          {/* Back Button (top right) */}
+          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+            <CustomButton title="Go Back" type="filled" handleClick={() => state.intro = true} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
+          </motion.div>
+
+          {/* Filter/Toggles (bottom) */}
+          <motion.div className='filtertabs-container' {...slideAnimation('up')}>
+          {FilterTabs.map(tab => (
+            <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" handleClick={() => {}}></Tab>
+          ))}
+
+
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
