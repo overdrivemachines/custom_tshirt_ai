d137a0ca7cccf408c3f35d3ceddde787855d2646
Dipen Chauhan <get.dipen@gmail.com>
2023-07-23 15:26:27 -0700

    fix file upload not showing

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import CustomButton from "./CustomButton";
 
-export const FilePicker = (file, setFile, readFile) => {
+export const FilePicker = ({ file, setFile, readFile }) => {
   return (
     <div className="filepicker-container">
       <div className="flex-1 flex flex-col">
         {/* Display file input and select the first file  */}
         <input type="file" id="file-upload" accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
-        <label htmlFor="filepicker-label">Upload File</label>
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
         <p className="mt-2 text-gray-500 text-xs truncate"> {typeof file.name == "undefined" ? "No file selected" : file.name}</p>
       </div>
       <div className="mt-4 flex flex-wrap gap-3">
