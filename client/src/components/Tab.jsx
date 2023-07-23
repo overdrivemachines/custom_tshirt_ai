b7bf591e747496e10ee2bd579a49772ddf2f355f
Dipen Chauhan <get.dipen@gmail.com>
2023-07-23 12:39:53 -0700

    clicking on the left tabs shows corresponding components

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { useSnapshot } from 'valtio';
+import React from "react";
+import { useSnapshot } from "valtio";
 
-import state from '../store';
+import state from "../store";
 
-export const Tab = ({tab, isFilterTab, isActiveTab, handleClick}) => {
+export const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
 
-  const activeStyles = isFilterTab && isActiveTab
-  ? {
-      backgroundColor: snap.color,
-      opacity: 0.5
-    }
-  : {
-      backgroundColor: "transparent",
-      opacity: 1
-    }
-
-  const tabClassName = isFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12 object-contain"
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? {
+          backgroundColor: snap.color,
+          opacity: 0.5,
+        }
+      : {
+          backgroundColor: "transparent",
+          opacity: 1,
+        };
+  const tabClassName = `tab-btn ${isFilterTab ? "rounded-full glassmorphism" : "rounded-4"}`;
+  const tabImageClassName = isFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12 object-contain";
   return (
-    <div key={tab.name} className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'} `} style={activeStyles}>
-      <img src={tab.icon} alt={tab.name} className={tabClassName} />
+    <div key={tab.name} className={tabClassName} style={activeStyles} onClick={handleClick}>
+      <img src={tab.icon} alt={tab.name} className={tabImageClassName} />
     </div>
-  )
-}
-export default Tab
+  );
+};
+export default Tab;
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,50 +1,79 @@
-import React, {useState, useEffect} from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useSnapshot } from 'valtio';
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
 
-import config from '../config/config'
-import state from '../store'
-import {download} from '../assets'
-import {downloadCanvasToImage, reader} from '../config/helpers';
-import {EditorTabs, FilterTabs, DecalTypes} from '../config/constants'
-import {fadeAnimation, slideAnimation} from '../config/motion'
+import config from "../config/config";
+import state from "../store";
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
 
-import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from "../components";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState("");
+
+  // AI Prompt
+  const [prompt, setPrompt] = useState("");
+
+  // Loading State. Are we currently generating an image?
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  // Active State
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab content depending on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
         <>
           {/* Tabs on the left */}
-          <motion.div key="custom" className='absolute top-0 left-0 z-10' {...slideAnimation('left')}>
+          <motion.div key="custom" className="absolute top-0 left-0 z-10" {...slideAnimation("left")}>
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
-                {EditorTabs.map(tab => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}}></Tab>
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => setActiveEditorTab(tab.name)} />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
 
           {/* Back Button (top right) */}
-          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
-            <CustomButton title="Go Back" type="filled" handleClick={() => state.intro = true} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
+          <motion.div className="absolute z-10 top-5 right-5" {...fadeAnimation}>
+            <CustomButton title="Go Back" type="filled" handleClick={() => (state.intro = true)} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
           </motion.div>
 
           {/* Filter/Toggles (bottom) */}
-          <motion.div className='filtertabs-container' {...slideAnimation('up')}>
-          {FilterTabs.map(tab => (
-            <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" handleClick={() => {}}></Tab>
-          ))}
-
-
+          <motion.div className="filtertabs-container" {...slideAnimation("up")}>
+            {FilterTabs.map((tab) => (
+              <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" handleClick={() => {}}></Tab>
+            ))}
           </motion.div>
         </>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Customizer
+export default Customizer;
