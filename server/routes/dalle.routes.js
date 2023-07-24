f3ea3e2816c2b919d9c849a5b1ba6353f63a60e5
Dipen Chauhan <get.dipen@gmail.com>
2023-07-23 17:05:41 -0700

    setting up openai

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -4,6 +4,12 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 const router = express.Router();
 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
